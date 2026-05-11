01a3a2516c42467aa90fc4bf69304a04ba992e6f
Shunichi Ikegami <sike.tm@gmail.com>
2026-05-11 18:39:16 +0900

    ActionMailer をテストで使う

--- spec/html2pdf/rails/mailer_rendering_spec.rb
+++ spec/html2pdf/rails/mailer_rendering_spec.rb
@@ -1,78 +1,75 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'action_mailer'
+
+class TestMailer < ActionMailer::Base
+  self.view_paths = [File.expand_path('../../fixtures/views', __dir__)]
+  self.delivery_method = :test
+
+  def notify(name, **opts)
+    @name = name
+    attachments['notify.pdf'] = render_to_pdf_string(**opts)
+    mail(to: 'to@example.com', from: 'from@example.com', subject: 'subject')
+  end
+end
 
 RSpec.describe Html2Pdf::Rails::MailerRendering do
-  let(:mailer_class) do
-    Class.new do
-      include Html2Pdf::Rails::MailerRendering
-
-      def self.mailer_name
-        'order_mailer'
-      end
-
-      attr_accessor :action_name, :render_to_string_calls
-
-      def initialize(action_name)
-        @action_name = action_name
-        @render_to_string_calls = []
-      end
-
-      def render_to_string(**opts)
-        @render_to_string_calls << opts
-        '<html>rendered</html>'
-      end
-    end
-  end
-
-  let(:mailer) { mailer_class.new('receipt') }
   let(:pdf_bytes) { "%PDF-1.4\nbinary".b }
+  let(:captured_html) { [] }
 
   before do
-    allow(Html2Pdf::Rails::Client).to receive(:post).and_return(pdf_bytes)
-  end
-
-  it 'returns the PDF bytes' do
-    expect(mailer.render_to_pdf_string).to eq(pdf_bytes)
-  end
-
-  it 'auto-detects template from mailer_name and action_name' do
-    mailer.render_to_pdf_string
-
-    expect(mailer.render_to_string_calls.last).to include(template: 'order_mailer/receipt')
-  end
-
-  it 'defaults formats to [:pdf]' do
-    mailer.render_to_pdf_string
-
-    expect(mailer.render_to_string_calls.last).to include(formats: [:pdf])
-  end
-
-  it 'defaults layout to false' do
-    mailer.render_to_pdf_string
-
-    expect(mailer.render_to_string_calls.last).to include(layout: false)
-  end
-
-  it 'allows overriding template, layout, and formats' do
-    mailer.render_to_pdf_string(template: 'custom/path', layout: 'pdf', formats: [:html])
-
-    expect(mailer.render_to_string_calls.last).to include(
-      template: 'custom/path',
-      layout: 'pdf',
-      formats: [:html]
-    )
-  end
-
-  it 'passes pdf_options through to Html2Pdf::Rails.generate' do
-    received_args = nil
-    allow(Html2Pdf::Rails::Client).to receive(:post) do |**kwargs|
-      received_args = kwargs
+    Html2Pdf.configure { |c| c.endpoint = 'https://example.com/pdf' }
+    allow(Html2Pdf::Rails::Client).to receive(:post) do |html:, **|
+      captured_html << html
       pdf_bytes
     end
+  end
 
-    mailer.render_to_pdf_string(pdf_options: { margin: { top: '30px' } })
+  describe '#render_to_pdf_string with default options' do
+    it 'returns the PDF bytes generated from the auto-detected template' do
+      mail = TestMailer.notify('World').deliver_now
 
-    expect(received_args).to include(html: '<html>rendered</html>', pdf_options: { margin: { top: '30px' } })
+      expect(mail.attachments['notify.pdf']).not_to be_nil
+      expect(mail.attachments['notify.pdf'].body.decoded).to eq(pdf_bytes)
+    end
+
+    it 'renders the .pdf.erb template (not .html.erb or .text.erb)' do
+      TestMailer.notify('World').deliver_now
+
+      expect(captured_html.last).to include('PDF template: Hello World!')
+      expect(captured_html.last).not_to include('HTML template')
+      expect(captured_html.last).not_to include('Email body text')
+    end
+  end
+
+  describe 'overriding formats' do
+    it 'renders the .html.erb template when formats: [:html] is passed' do
+      TestMailer.notify('World', formats: [:html]).deliver_now
+
+      expect(captured_html.last).to include('HTML template: Hello World!')
+    end
+  end
+
+  describe 'overriding template' do
+    it 'renders the specified template' do
+      TestMailer.notify('World', template: 'test_mailer/custom').deliver_now
+
+      expect(captured_html.last).to include('Custom template body')
+    end
+  end
+
+  describe 'passing pdf_options through' do
+    it 'forwards pdf_options to Html2Pdf::Rails.generate' do
+      received = nil
+      allow(Html2Pdf::Rails::Client).to receive(:post) do |**kwargs|
+        received = kwargs
+        pdf_bytes
+      end
+
+      TestMailer.notify('World', pdf_options: { margin: { top: '30px' } }).deliver_now
+
+      expect(received).to include(pdf_options: { margin: { top: '30px' } })
+    end
   end
 end
