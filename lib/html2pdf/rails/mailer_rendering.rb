57846aab4e67528a81f352830e8368c6f09e16cd
Shunichi Ikegami <sike.tm@gmail.com>
2026-05-13 09:13:14 +0900

    render_to_pdf_string のデフォルト template の決定を rails にまかせる

--- lib/html2pdf/rails/mailer_rendering.rb
+++ lib/html2pdf/rails/mailer_rendering.rb
@@ -4,7 +4,9 @@ module Html2Pdf
   module Rails
     module MailerRendering
       def render_to_pdf_string(template: nil, layout: false, formats: [:pdf], handlers: nil, pdf_options: {})
-        template ||= File.join(self.class.mailer_name, action_name)
+        # When :template is omitted, Rails resolves the template from `_prefixes`
+        # (= [mailer_name]) and `action_name` via `_process_render_template_options`:
+        # https://github.com/rails/rails/blob/v8.1.2/actionview/lib/action_view/rendering.rb#L177
         render_opts = { template: template, layout: layout, formats: formats, handlers: handlers }.compact
         html = render_to_string(**render_opts)
         Html2Pdf::Rails.generate(html: html, pdf_options: pdf_options)
